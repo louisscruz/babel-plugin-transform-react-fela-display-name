cd063a6f4d66aceb47dd2cf84b096a39c35fad01
Louis Cruz <lcruz@zendesk.com>
2018-01-16 23:27:40 -0800

    Handle destructured imports

--- src/index.js
+++ src/index.js
@@ -1,23 +1,97 @@
 const transformReactFelaDisplayName = ({ types: t }) => {
   const functionNameRegEx = /^createComponent(WithProxy)?$/;
+
+  const injectDisplayName = (initialLineNodePath, componentName) => {
+    const left = t.memberExpression(t.identifier(componentName), t.identifier('displayName'));
+    const right = t.stringLiteral(componentName);
+    const displayNameAssignment = t.assignmentExpression('=', left, right);
+    initialLineNodePath.insertAfter(displayNameAssignment);
+  };
+
+  const getFunctionBinding = (path, callee) => {
+    const { scope: { bindings } } = path;
+    return bindings[callee.name];
+  };
+
+  const identifierComesFromReactFela = (identifierDeclarationPath, calleeName) => {
+    const reactFelaRegEx = /react-fela(\/index(\.js)?)?$/;
+    const { scope: { bindings } } = identifierDeclarationPath;
+    const sourcePath = bindings[calleeName].path;
+
+    if (sourcePath.isImportSpecifier() && sourcePath.parentPath.isImportDeclaration()) {
+      // Handle cases where the function is imported destructured. For example:
+      //
+      // import { createComponent } from 'react-fela';
+      // /* or */
+      // import { createComponentWithProxy }j from 'react-fela';
+      //
+      const {
+        parent: { source: { value: sourceImportFrom } },
+        node: { imported: { name: importedName } }
+      } = sourcePath;
+      const isFromReactFela = reactFelaRegEx.test(sourceImportFrom);
+      const validImportedName = functionNameRegEx.test(importedName);
+      return isFromReactFela && validImportedName;
+    }
+    return false;
+  };
+
   return {
     name: 'transform-react-fela-display-name',
     visitor: {
-      VariableDeclarator: {
-        enter(path) {
-          const { node: { id, init, init: { callee } } } = path;
-          if (t.isCallExpression(init) && callee.name.match(functionNameRegEx)) {
-            const componentName = id.name;
-            const left = t.memberExpression(
-              t.identifier(componentName),
-              t.identifier('displayName')
-            );
-            const right = t.stringLiteral(componentName);
-            const displayNameAssignment = t.assignmentExpression('=', left, right);
-            path.parentPath.insertAfter(displayNameAssignment);
+      // Match cases such as:
+      //
+      // const x = y;
+      //
+      VariableDeclarator(path) {
+        const { node: { id, init, init: { callee } } } = path;
+
+        // Match cases such as:
+        //
+        // const x = y();
+        //
+        if (t.isCallExpression(init)) {
+          const componentName = id.name;
+          const initialLineNodePath = path.parentPath;
+
+          // Match cases such as:
+          //
+          // const x = createComponent(...);
+          // /* or */
+          // const y = createComponentWithProxy(...);
+          //
+          if (
+            callee.name.match(functionNameRegEx) &&
+            identifierComesFromReactFela(path, callee.name)
+          ) {
+            injectDisplayName(initialLineNodePath, componentName);
+          } else {
+            const functionBinding = getFunctionBinding(path, callee);
+            const { path: { node: { init: bindingInit } } } = functionBinding;
+
+            // This handles renaming of the createComponent functions. For example:
+            //
+            // import { createComponent } from 'react-fela';
+            // const renameIt = createComponent;
+            // const MyComponent = renameIt(...);
+            //
+            // console.log(bindingInit);
+            if (
+              t.isIdentifier(bindingInit) &&
+              identifierComesFromReactFela(
+                functionBinding.path,
+                bindingInit.name
+              ) /* path.parent.declarations[0]) */
+            ) {
+              injectDisplayName(initialLineNodePath, componentName);
+            }
           }
         }
       }
+      // ImportDeclaration(path) {
+      //   console.log(Object.keys(path));
+      //   console.log(path.node.source);
+      // }
     }
   };
 };
--- test/index.js
+++ test/index.js
@@ -33,8 +33,47 @@ pluginTester({
     },
     'does not add a line for functions that do not match the correct name': `
       import { createComponentWithProxy } from 'react-fela';
+      const createComponentWithSomeOtherThing = el => el;
       const MyComponentRules = () => ({});
       const MyComponent = createComponentWithSomeOtherThing(MyComonentRules, 'div');
-    `
+    `,
+    'does not add a line for non-react-fela createComponent function calls': `
+      const createComponent = el => el;
+      const response = createComponent('test');
+    `,
+    'does not add a line for non-react-fela createComponentWithProxy function calls': `
+      const createComponentWithProxy = el => el;
+      const response = createComponentWithProxy('test');
+    `,
+    'adds a line setting the displayName in scenarios where createComponent is used through one intermediary variable': {
+      code: `
+        import { createComponent } from 'react-fela';
+        const intermediaryVariable = createComponent;
+        const MyComponentRules = () => ({});
+        const MyComponent = intermediaryVariable(MyComonentRules, 'div');
+      `,
+      output: `
+        import { createComponent } from 'react-fela';
+        const intermediaryVariable = createComponent;
+        const MyComponentRules = () => ({});
+        const MyComponent = intermediaryVariable(MyComonentRules, 'div');
+        MyComponent.displayName = 'MyComponent'
+      `
+    },
+    'adds a line setting the displayName in scenarios where createComponentWithProxy is used through one intermediary variable': {
+      code: `
+        import { createComponentWithProxy } from 'react-fela';
+        const intermediaryVariable = createComponentWithProxy;
+        const MyComponentRules = () => ({});
+        const MyComponent = intermediaryVariable(MyComonentRules, 'div');
+      `,
+      output: `
+        import { createComponentWithProxy } from 'react-fela';
+        const intermediaryVariable = createComponentWithProxy;
+        const MyComponentRules = () => ({});
+        const MyComponent = intermediaryVariable(MyComonentRules, 'div');
+        MyComponent.displayName = 'MyComponent'
+      `
+    }
   }
 });
